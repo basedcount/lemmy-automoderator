65153f5ff2ad85680b5245660ed328e16116d82f
Paolo Sernini <tommy.ornato@gmail.com>
2023-06-18 17:37:01 +0200

    Lemmy bot skeleton This bot outputs every comment to the console

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import LemmyBot from 'lemmy-bot';
+import 'dotenv/config'
+
+const bot = new LemmyBot({
+    credentials: {
+        username: process.env.LEMMY_USERNAME || '',
+        password: process.env.LEMMY_PASSWORD || '',
+    },
+    instance: 'forum.basedcount.com',
+    connection: {
+        minutesBeforeRetryConnection: 1,
+        secondsBetweenPolls: 5
+    },
+    federation: 'local',    //Do not access content outside of the local server
+    handlers: {
+        comment: ({
+            commentView: {
+                comment: { content },
+                creator: { name }
+            },
+            // botActions: {  }
+        }) => {
+            console.log(`${name} says: "${content}"`);
+        },
+    },
+    dbFile: 'db.sqlite3',    //Path for the SQLite DB. Used by the bot's lib internally  
+});
+
+bot.start();
