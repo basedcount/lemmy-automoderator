e601114dfe8f3c6944dd418966ef0891b7ed4af9
Tommaso Ornato <tommy.ornato@gmail.com>
2023-07-02 16:33:22 +0200

    Fetching modlist for hardcoded communities at bot start and every hour (cron)

---
+++ src/api.ts
@@ -0,0 +1,30 @@
+import { LemmyHttp, Login } from 'lemmy-js-client';
+
+//Logs in through the Lemmy JS client, returns a client and its JWT
+export async function login(username: string, password: string, instance: string) {
+    const client: LemmyHttp = new LemmyHttp('https://' + instance);
+    const loginForm: Login = {
+        username_or_email: username,
+        password: password,
+    };
+
+    const jwt = (await client.login(loginForm)).jwt;
+
+    return { client, jwt };
+}
+
+//Returns a list of actor_ids (https://INSTANCE/u/USERNAME) of all the moderators
+export async function getModList(client: LemmyHttp, communityName: string) {
+    const community = await client.getCommunity({ name: communityName });
+    const mods = community.moderators.map(mod => ({ 
+        actor_id: mod.moderator.actor_id, 
+        community_id: community.community_view.community.id 
+    })) satisfies Moderator[];
+
+    return mods;
+}
+
+export interface Moderator {
+    actor_id: string;
+    community_id: number;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,87 @@
+import 'dotenv/config';
 import LemmyBot from 'lemmy-bot';
-import 'dotenv/config'
+import { login, getModList, Moderator } from './api';
+import cron from 'node-cron';
 
-// DOCS: https://github.com/SleeplessOne1917/lemmy-bot#lemmybotoptions
-// DISCUSSION: https://github.com/LemmyNet/lemmy/issues/3281
+const USERNAME = process.env.LEMMY_USERNAME || '';
+const PASSWORD = process.env.LEMMY_PASSWORD || '';
+const INSTANCE = process.env.LEMMY_INSTANCE || '';
+const DATABASE = 'db.sqlite3';
+let mods: Moderator[] = [];
 
-const bot = new LemmyBot({
-    credentials: {
-        username: process.env.LEMMY_USERNAME || '',
-        password: process.env.LEMMY_PASSWORD || '',
-    },
-    instance: 'forum.basedcount.com',
-    connection: {
-        secondsBetweenPolls: 2,
-    },
-    federation: 'local',    //Do not access content outside of the local server
-    handlers: {
-        comment: async ({
-            commentView: {
-                comment: { id: commentId, content: commentText },
-                creator: { id: userId, name: userName },
-                post: {id: postId, name: postTitle }
-            },
-            botActions: {  
-                createComment,
-                reportComment,
-                reportPost,
-                removeComment,
-                lockPost,
-                sendPrivateMessage,
-                resolveCommentReport
-                // featurePost (pin post - doesn't appear to work)
-            }
-        }) => {
-            console.log(`${userName} says: "${commentText}"`);
+const communities = ['pcm'];    //Real implementation: store these in the local sqlite database
+
+if (USERNAME === '' || PASSWORD === '' || INSTANCE === '') {
+    throw new Error('Undefined username, password or instance. Check your .env file.');
+}
+
+cron.schedule('0 * * * *', refreshMods);    //Refresh mod list every hour
+
+run();
+
+async function run() {
+    await refreshMods();
+
+    const bot = new LemmyBot({
+        credentials: {
+            username: USERNAME,
+            password: PASSWORD,
         },
-        //post (check submission title, link...)
-        //privateMessage (configuration if moderator)
-        //mention (do stuff if mentioned by mod)
-        //commentReport (if possible count report, remove if has X more report) 
-        //postReport (if possible count report, remove if has X more report) 
-        //privateMessageReport (if possible count report, remove if has X more report) 
-        //modLockPost ("Post locked because...")
-        //modBanFromCommunity ("You have been banned because...")
-    },
-    dbFile: 'db.sqlite3',    //Path for the SQLite DB. Used by the bot's lib internally  
-});
+        instance: INSTANCE,
+        connection: {
+            secondsBetweenPolls: 2,
+        },
+        federation: 'local',    //Do not access content outside of the local server
+        handlers: {
+            comment: async ({
+                commentView: {
+                    comment: { id: commentId, content: commentText },
+                    creator: { id: userId, name: userName },
+                    post: { id: postId, name: postTitle }
+                },
+                botActions: {
+                    createComment,
+                    reportComment,
+                    reportPost,
+                    removeComment,
+                    lockPost,
+                    sendPrivateMessage,
+                    resolveCommentReport
+                    // featurePost (pin post - doesn't appear to work)
+                }
+            }) => {
+                console.log(`${userName} says: "${commentText}"`);
+                console.log('Mods:', mods);
+            },
+            //post (check submission title, link...)
+            //privateMessage (configuration if moderator)
+            //mention (do stuff if mentioned by mod)
+            //commentReport (if possible count report, remove if has X more report) 
+            //postReport (if possible count report, remove if has X more report) 
+            //privateMessageReport (if possible count report, remove if has X more report) 
+            //modLockPost ("Post locked because...")
+            //modBanFromCommunity ("You have been banned because...")
+        },
+        dbFile: DATABASE
+    });
 
-bot.start();
+    bot.start();
+}
+
+//Login and retrieve the most recent mod list
+async function refreshMods() {
+    try{
+        const { client } = await login(USERNAME, PASSWORD, INSTANCE);
+        
+        let localMods: Moderator[] = [];
+        for (const community of communities) {
+            const modList = await getModList(client, community);
+
+            localMods = localMods.concat(modList);
+        }
+        
+        mods = localMods;
+    } catch(e) {
+        console.log('Error while refreshing mod list:', e);
+    }
+}
